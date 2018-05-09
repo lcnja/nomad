c40000f02f54d2f6834b427eeb6fd8efa3b40209
Michael Lange <dingoeatingfuzz@gmail.com>
2018-05-08 18:08:30 -0700

    New accordion component
    Follows the same style as the table and pagination components.

---
+++ ui/app/components/list-accordion.js
@@ -0,0 +1,39 @@
+import Component from '@ember/component';
+import { computed, get, set } from '@ember/object';
+
+export default Component.extend({
+  classNames: ['accordion'],
+
+  key: 'id',
+  source: computed(() => []),
+
+  decoratedSource: computed('source.[]', function() {
+    const stateCache = this.get('stateCache');
+    const key = this.get('key');
+    const deepKey = `item.${key}`;
+
+    const decoratedSource = this.get('source').map(item => {
+      const cacheItem = stateCache.findBy(deepKey, get(item, key));
+      return {
+        item,
+        isOpen: cacheItem ? !!cacheItem.isOpen : false,
+      };
+    });
+
+    this.set('stateCache', decoratedSource);
+    return decoratedSource;
+  }),
+
+  // When source updates come in, the state cache is used to preserve
+  // open/close state.
+  stateCache: computed(() => []),
+
+  actions: {
+    open(item) {
+      set(item, 'isOpen', true);
+    },
+    close(item) {
+      set(item, 'isOpen', false);
+    },
+  },
+});
